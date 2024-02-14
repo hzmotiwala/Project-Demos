052abe8ea5a8e0de950582e78bbcb1fc777afdc9
hzmotiwala <zanemotiwala@gmail.com>
2024-02-13 23:53:02 -0800

    Update streamlit-llm-youtube.py
    added loading spinner

--- llm-youtube-summary/streamlit-llm-youtube.py
+++ llm-youtube-summary/streamlit-llm-youtube.py
@@ -39,30 +39,30 @@ def summarize_video_content(youtube_url):
             # Fetch the transcript of the YouTube video
             transcript = get_transcript_from_youtube(youtube_url)
             if transcript:
-
-                 # Check if the total number of tokens in the transcript exceeds the threshold
+                # Check if the total number of tokens in the transcript exceeds the threshold
                 max_tokens_threshold = 5000
                 if len(transcript.split()) > max_tokens_threshold:
                     st.error("Transcript is too long. Please pick a shorter video.")
                     return None
-
-                # Load the summarization model
-                summarization_model = pipeline("summarization", model="facebook/bart-large-cnn")
-                
-                # Split the transcript into smaller chunks
-                chunk_size = 3000  # Adjust the chunk size as needed
-                transcript_chunks = [transcript[i:i+chunk_size] for i in range(0, len(transcript), chunk_size)]
-                
-                # Summarize each chunk and concatenate the summaries
-                summaries = []
-                for chunk in transcript_chunks:
-                    summary = summarization_model(chunk, max_length=300, min_length=50, do_sample=False)
-                    summaries.append(summary[0]['summary_text'])
-                
-                # Concatenate the summaries of all chunks
-                final_summary = " ".join(summaries)
-                
-                return final_summary
+                # Show a loading spinner while the transcript is being summarized
+                with st.spinner("Summarizing transcript..."):
+                    # Load the summarization model
+                    summarization_model = pipeline("summarization", model="facebook/bart-large-cnn")
+                    
+                    # Split the transcript into smaller chunks
+                    chunk_size = 3000  # Adjust the chunk size as needed
+                    transcript_chunks = [transcript[i:i+chunk_size] for i in range(0, len(transcript), chunk_size)]
+                    
+                    # Summarize each chunk and concatenate the summaries
+                    summaries = []
+                    for chunk in transcript_chunks:
+                        summary = summarization_model(chunk, max_length=300, min_length=50, do_sample=False)
+                        summaries.append(summary[0]['summary_text'])
+                    
+                    # Concatenate the summaries of all chunks
+                    final_summary = " ".join(summaries)
+                    
+                    return final_summary
             else:
                 st.warning("Failed to fetch transcript. Please check the YouTube URL.")
                 return None
